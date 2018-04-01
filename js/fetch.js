774126aff1a0e7cdde93fcb0fa1524c6a4ee1e4a
Monis Ahmad <monis.ahmad42@gmail.com>
2018-04-01 16:58:06 +0530

    basic js is working

--- js/fetch.js
+++ js/fetch.js
@@ -1,37 +1,49 @@
   (function () {
     var container = document.querySelector(".container");
-    reddit.top("funny").t("day").limit(10).fetch(function (res) {
+    reddit.top("funny").t("day").limit(1000).fetch(function (res) {
       for (var i = 0; i < res.data.children.length; i++) {
         var awwData = res.data.children[i].data;
-        // var thumbnail = document.createElement("img");
-        // var aww = document.createElement("img");
-        // var row = document.createElement("div");
-        // var left = document.createElement("div");
-        // var right = document.createElement("div");
-        // var link = document.createElement("a");
-        // row.className = "row";
-        // left.className = "col-xs-4";
-        // thumbnail.setAttribute("src", awwData.thumbnail);
-        // link.setAttribute("href", "http://www.reddit.com" + awwData.permalink);
-        // link.innerText = awwData.title;
-        // left.appendChild(thumbnail);
-        // left.appendChild(link);
-        // row.appendChild(left);
-        // right.className = "col-xs-8";
-        // aww.className = "img-responsive";
-        // aww.setAttribute("src", awwData.url);
-        // right.appendChild(aww);
-        // row.appendChild(right);
-        var aww = document.createElement("img");
+        if(awwData.url.search("gif")>0){
+                var aww = document.createElement("video");
         //var row = document.createElement("div");
         var box =document.createElement("div");
         //row.className ="row";
+        var vidsrc=document.createElement("source");
         box.className ="col-lg-4 col-sm-6 col-xs-12";
+        
+        var url=awwData.url.replace('gifv','mp4')
+
+        vidsrc.setAttribute("src" , url);
+        aww.setAttribute("preload","auto");
+        aww.setAttribute("autoplay","autoplay");
+        aww.setAttribute("loop","loop")
+        vidsrc.setAttribute("type","video/mp4");
+        aww.className="img-responsive";
+        aww.appendChild(vidsrc)
+        box.appendChild(aww);
+        //row.appendChild(box);
+        container.appendChild(box);}
+        else if(awwData.url.search("v.redd")>0){}
+        else if(awwData.url.search("giant")>0){}    
+        else if(awwData.url.search("imgur")>0){}
+        else{
+        //creating div for videos
+                var aww = document.createElement("img");
+        //var row = document.createElement("div");
+        var box =document.createElement("div");
+        var vidsrc=document.createElement("source");
+        //row.className ="row";
+        box.className ="col-lg-4 col-sm-6 col-xs-12";
+        
+        
+
         aww.setAttribute("src" , awwData.url);
         aww.className="img-responsive";
         box.appendChild(aww);
         //row.appendChild(box);
-        container.appendChild(box);
+        container.appendChild(box);}
+
+
       }
     });
   })()
